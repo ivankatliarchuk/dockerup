c0ccc58dd8832cc1e16abf14ef41f8a46365650f
Ivan Katliarchuk <ivan.katliarchuk@midlandhr.co.uk>
2016-12-19 00:42:35 +0000

    migrate to settings for every each environment. :IvKa

--- withansible/todobackend/src/manage.py
+++ withansible/todobackend/src/manage.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
+    # shows which specific application settings allows to set specific application settings
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todobackend.settings.base")
 
     from django.core.management import execute_from_command_line
