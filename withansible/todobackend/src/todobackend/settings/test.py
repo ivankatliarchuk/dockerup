2d2976bf413a29b828b127aaa0f9365c930e31fb
Ivan Katliarchuk <tanzwud.work@gmail.com>
2016-12-20 01:40:35 +0000

    cache test in dockerized environment.

--- withansible/todobackend/src/todobackend/settings/test.py
+++ withansible/todobackend/src/todobackend/settings/test.py
@@ -8,7 +8,7 @@ TEST_OUTPUT_DIR = os.environ.get('TEST_OUTPUT_DIR','.')
 NOSE_ARGS = [
   '--verbosity=2',                  # verbose output
   '--nologcapture',                 # don't output log capture
-  '--with-coverage',                # activate coverage report
+  '--with-coverage',                 # activate coverage report
   '--cover-package=todo',           # coverage reports will apply to these packages
   '--with-spec',                    # spec style tests
   '--spec-color',
@@ -18,15 +18,15 @@ NOSE_ARGS = [
   '--cover-xml-file=%s/coverage.xml' % TEST_OUTPUT_DIR,
 ]
 
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.environ.get('MYSQL_DATABASE','todobackend'),
-#         'USER': os.environ.get('MYSQL_USER','todo'),
-#         'PASSWORD': os.environ.get('MYSQL_PASSWORD','password'),
-#         'HOST': os.environ.get('MYSQL_HOST','localhost'),
-#         'PORT': os.environ.get('MYSQL_PORT','3306'),
-#     }
-# }
+#Database
+#https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE','todobackend'),
+        'USER': os.environ.get('MYSQL_USER','todo'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD','password'),
+        'HOST': os.environ.get('MYSQL_HOST','localhost'),
+        'PORT': os.environ.get('MYSQL_PORT','3306'),
+    }
+}
