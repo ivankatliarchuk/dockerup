41403e19f45ce4ebeda6cf800f0cb8b28bd42489
Ivan Katliarchuk <tanzwud.work@gmail.com>
2016-12-20 00:59:24 +0000

    craete requirements files .

--- withansible/todobackend/src/todobackend/settings/test.py
+++ withansible/todobackend/src/todobackend/settings/test.py
@@ -1,11 +1,10 @@
-import os
-
 from base import *
 
 # Installed Apps
 INSTALLED_APPS += ('django_nose', )
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 TEST_OUTPUT_DIR = os.environ.get('TEST_OUTPUT_DIR','.')
+# specify arguments for nose
 NOSE_ARGS = [
   '--verbosity=2',                  # verbose output
   '--nologcapture',                 # don't output log capture
@@ -21,13 +20,13 @@ NOSE_ARGS = [
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_DATABASE','todobackend'),
-        'USER': os.environ.get('MYSQL_USER','todo'),
-        'PASSWORD': os.environ.get('MYSQL_PASSWORD','password'),
-        'HOST': os.environ.get('MYSQL_HOST','localhost'),
-        'PORT': os.environ.get('MYSQL_PORT','3306'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.environ.get('MYSQL_DATABASE','todobackend'),
+#         'USER': os.environ.get('MYSQL_USER','todo'),
+#         'PASSWORD': os.environ.get('MYSQL_PASSWORD','password'),
+#         'HOST': os.environ.get('MYSQL_HOST','localhost'),
+#         'PORT': os.environ.get('MYSQL_PORT','3306'),
+#     }
+# }
