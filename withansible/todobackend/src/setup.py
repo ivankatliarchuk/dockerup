0a5270e6f4363e5b5139b7fe00205ce6d94e2d49
Ivan Katliarchuk <tanzwud.work@gmail.com>
2016-12-21 00:26:18 +0000

    update minimum and max dependencies releases.

--- withansible/todobackend/src/setup.py
+++ withansible/todobackend/src/setup.py
@@ -8,18 +8,18 @@ setup (
     include_package_data    = True,
     scripts                 = ["manage.py"],
     install_requires       = [
-        "Django==1.10.4",
-        "django-cors-headers==1.3.1",
-        "djangorestframework==3.5.3",
-        "MySQL-python==1.2.5"
+        "Django>=1.10.4,<2.0",
+        "django-cors-headers>=1.3.1",
+        "djangorestframework>=3.5.3",
+        "MySQL-python>=1.2.5"
     ],
     extras = {
         "test": [
-            "colorama==0.3.7",
-            "coverage==4.2",
-            "django-nose==1.4.4",
-            "nose==1.3.7",
-            "pinocchio==0.4.2"
+            "colorama>=0.3.7",
+            "coverage>=4.2",
+            "django-nose>=1.4.4",
+            "nose>=1.3.7",
+            "pinocchio>=0.4.2"
         ]
     }
 )
